e5f1de82df98fcfaa9f5c5ca49782636d92c5c78
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-17 13:56:38 +0200

    build: add project base files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, 
+i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali 
+dei numeri da indovinare sono stati individuati.*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//visualizzare 5 numeri casuali in DOM
+
+//far partire un timer di 30 secondi
+
+//far sparire i numeri dopo i 30 secondi
+
+//chiedere all'utente quanti numeri ricorda della sequenza visualizzata
+
+//Se i numeri immessi corrispondono alla sequenza, comunicare quanti e quali numeri della sequenza sono stati individuati
